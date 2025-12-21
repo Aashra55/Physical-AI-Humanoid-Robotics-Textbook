7b1fa6612794aa574220f0fa43d83c6dd6eab618
Aashra Saleem <160631977+Aashra55@users.noreply.github.com>
2025-12-21 13:16:32 +0500

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,14 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-robotics.example.com',
+  url: 'https://Aashra55.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Physical-AI-Humanoid-Robotics-Textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'AI-Spec-Driven-Hackathon', // Usually your GitHub org/user name.
+  organizationName: 'Aashra55', // Usually your GitHub org/user name.
   projectName: 'Physical-AI-Humanoid-Robotics-Textbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
